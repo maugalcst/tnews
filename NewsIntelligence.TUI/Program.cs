8f50b8cd706a0c53278376b6e25dc1b85faea770
maugalcst <maugal.cst@gmail.com>
2026-06-15 22:30:15 -0600

    initial tui commit, using Terminal.Gui

---
+++ NewsIntelligence.TUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using Terminal.Gui;
+
+namespace NewsIntelligence.TUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Init();
+
+            var top = Application.Top;
+
+            var mainWindow = new Window("TNEWS - News Intelligence")
+            {
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+
+            top.Add(mainWindow);
+
+            Application.Run();
+            Application.Shutdown();
+        }
+    }
+}
